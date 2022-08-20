ec49793b3b787ec6481ca13994d80f314bfdafee
Jason Mann <aztuxmann+github@gmail.com>
2022-08-19 23:01:44 -0700

    Significant updates, final update.
    Finished commenting the code for better comprehension.
    Separated the timer into its own function.
    Turned on first LED when timer starts to give feedback when button is pressed.
    Added a lower power mode when idle instead of keeping LEDs on all the time.

--- ToothBrushTimer.ino
+++ ToothBrushTimer.ino
@@ -1,62 +1,73 @@
 /*
  * Tooth Brush Timer ( TBT )
  * 
- * The TBT powers on and then waits for the button to be pressed 
- * before counting up to two minutes. The LEDs will go from the lowest
- * green to yellow, then red. At the end, it will flash all LEDs a few 
- * times and lower all LEDs off.
+ * The TBT powers on. The 16 LEDs turn on one at a time going from the 
+ * first green to the last red and then backwards. Then the TBT waits 
+ * for the button to be pressed. While waiting, the TBT will cycle 
+ * through all the LEDs at 1/10th power. When the button is pushed, 
+ * the timer will run for about two minutes. At full brightness, the 
+ * LEDs will go from the lowest green to yellow, then red. At the end, 
+ * it will flash all LEDs a few times and lower all LEDs off.
  */
 
-const int buttonPin = 8;
-int       buttonState = 0;
+const int buttonPin   = 8;
+int  buttonState = 0;
+//                  G  G  G  G  G  G  G  G  G Y Y Y Y Y R R   Green,Yellow, Red.
 const int leds[] = {17,18,19,20,21,22,23,16,0,1,2,3,4,5,6,7};
-const int timer = 6000;
+const int timer  = 6000;
+const int ztimer = 300;
 
 void setup() {
-  for (int i=0; i<16; i++) {    // Set LEDs as outputs.
+  pinMode(buttonPin, INPUT_PULLUP); // Set internal pullup resistor.
+  for (int i=0; i<16; i++) {        // Set LEDs as outputs.
     pinMode(leds[i], OUTPUT);
   }
-  for (int i=0; i<16; i++) {    // Move lit LED bottom to top
+  for (int i=0; i<16; i++) {        // Move lit LED from bottom to top.
     digitalWrite(leds[i], HIGH);
-    delay(100);
+    delay(75);
     digitalWrite(leds[i], LOW);
   }
-  for (int i=15; i>=0; i--) {   // Move lit LED top to bottom
+  for (int i=15; i>=0; i--) {       // Move lit LED from top to bottom.
     digitalWrite(leds[i], HIGH);
-    delay(100);
+    delay(75);
     digitalWrite(leds[i], LOW);
   }
-  pinMode(buttonPin, INPUT_PULLUP);
 }
 
-void loop() {
-  buttonState = digitalRead(buttonPin);
-
-  // check if the pushbutton is pressed.
-  if (buttonState == LOW) {
-    for (int i=0; i<16; i++) {    // Start Toothbrush counting.
-      digitalWrite(leds[i], LOW);
-    }
-    for (int i=0; i<16; i++) {    // Turn on LEDs one at a time.
-      delay(timer);
-      digitalWrite(leds[i], HIGH);
-    }
-    for (int i=0; i<16; i++) {    // End of timer, flash LEDS
-      for (int i=0; i<16; i++) {    // Set all LEDs high.
+void loop() {  // Waiting for the button to be pressed.
+  for (int i=0; i<16; i++) {        // Cycle through LEDs at 1/10th power 
+    for (int j=0; j<ztimer; j++) {  // Count to ztimer value before going to next LED.
+      buttonState = digitalRead(buttonPin); // Check if the pushbutton is pressed.
+      if (buttonState == LOW) { // Start Timer for brushing teeth.
+        runtimer();
+      } else {                  // Continue waiting for  button press.
         digitalWrite(leds[i], HIGH);
-        delay(5);
-      }
-      delay(500);
-      for (int i=0; i<16; i++) {    // Set all LEDs low
+        delay(1);
         digitalWrite(leds[i], LOW);
-        delay(5);
+        delay(10);
       }
-      delay(500);
     }
-  } else {
-    digitalWrite(leds[0], HIGH);
-    delay(1);
-    digitalWrite(leds[0], LOW);
-    delay(10);
+  }
+}
+
+void runtimer(){                // Start Toothbrush counting.
+  for (int i=0; i<16; i++) {    // Set all LEDs low
+    digitalWrite(leds[i], LOW);
+  }
+  for (int i=0; i<16; i++) {    // Turn on LEDs one at a time.
+    digitalWrite(leds[i], HIGH);
+    delay(timer);
+  }
+  for (int i=0; i<16; i++) {    // End of timer, flash LEDS
+    for (int i=0; i<16; i++) {    // Set all LEDs high.
+      digitalWrite(leds[i], HIGH);
+      delay(5);
+    }
+    delay(500);
+    for (int i=0; i<16; i++) {    // Set all LEDs low
+      digitalWrite(leds[i], LOW);
+      delay(5);
+    }
+    delay(500);
   }
 }
